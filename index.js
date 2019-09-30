43413de0ea58cb5992749c281521805eff71db82
maltrana <fer@ticmedia.com>
2019-09-30 20:20:28 +0200

    Graba y lee los mensajes del usuario

--- index.js
+++ index.js
@@ -58,4 +58,68 @@ const uiConfig = {
   }
 };
 
-// const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+// Listen to RSVP button clicks
+startRsvpButton.addEventListener("click",
+ () => {
+   
+     if (firebase.auth().currentUser) {
+      // User is signed in; allows user to sign out
+      firebase.auth().signOut();
+    } else {
+      // No user is signed in; allows user to sign in
+      ui.start("#firebaseui-auth-container", uiConfig);
+    }
+});
+
+
+// Listen to the current Auth state
+firebase.auth().onAuthStateChanged((user)=> {
+  if (user) {
+    startRsvpButton.textContent = "Salir";
+    guestbookContainer.style.display = "block";
+  }
+  else {
+    startRsvpButton.textContent = "Login";
+    guestbookContainer.style.display = "none";
+  }
+});
+
+
+// Listen to the form submission
+form.addEventListener("submit", (e) => {
+   // Prevent the default form redirect
+   e.preventDefault();
+ 
+   // Write a new message to the database collection "guestbook"
+   firebase.firestore().collection("guestbook").add({
+      text: input.value,
+      timestamp: Date.now(),
+      name: firebase.auth().currentUser.displayName,
+      userId: firebase.auth().currentUser.uid
+   })
+ 
+   // clear message input field
+   input.value = ""; 
+   
+   // Return false to avoid redirect
+   return false;
+});
+
+
+// Create query for messages
+firebase.firestore().collection("guestbook")
+  .orderBy("timestamp","desc")
+  .onSnapshot((snaps) => {
+       // Reset page
+       guestbook.innerHTML = "";
+      
+       // Loop through documents in database
+       snaps.forEach((doc) => {
+         // Create an HTML entry for each document and add it to the chat
+         const entry = document.createElement("p");
+         entry.textContent =  doc.data().name + " : " + doc.data().text;
+         guestbook.appendChild(entry);
+   });
+});
